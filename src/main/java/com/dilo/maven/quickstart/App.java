1b037205100af335d0245d354e89a483bf014ed4
Dilşen Yıldar <dilsenyildar@gmail.com>
2018-03-02 22:38:36 +0300

    v 1.1.2

--- src/main/java/com/dilo/maven/quickstart/App.java
+++ src/main/java/com/dilo/maven/quickstart/App.java
@@ -1,29 +1,43 @@
 package com.dilo.maven.quickstart;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.sql.SQLException;
 import java.util.HashSet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.AbstractHandler;
 
 /**
  *
  * @author Dilsen Yildar <dilsenyldr@yandex.com>
  */
-public class App {
+public class App extends AbstractHandler{
+	private static Logger logger = LogManager.getLogger();
 	static HashSet<LogAttributes> hsAttr = new HashSet<LogAttributes>();
-	public static void main(String[] args) throws UnsupportedEncodingException, IOException {
+	
+	public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
 		FileHandler fh = new FileHandler();
-		/**
-		 * This is where i convert to log inputs to json.
-		 */
 		fh.inputsFromLogFile(hsAttr);
-		/**
-		 * This is where i add to logInputs into postgresql db.
-		 */
 		addDB();
 		fh.deleteLogFile();
 		fh.createLogFile();
 	}
+	
+	public static void main(String[] args) throws Exception {
+		Server server = new Server(8080);
+		server.setHandler(new App());
+		server.start();
+		server.join();
+	}
+	
 	private static void addDB() {
 		LoggingInDB lidb = new LoggingInDB();
         try {
@@ -31,6 +45,7 @@ public class App {
         		lidb.createOp(la);
         	}
 		} catch (SQLException e) {
+			logger.error(e);
 			e.printStackTrace();
 		}
 	}
