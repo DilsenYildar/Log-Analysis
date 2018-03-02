58671ebbb887b7fe5abbcafd3b3aa854550d20cb
Dilşen Yıldar <dilsenyildar@gmail.com>
2018-03-02 20:01:11 +0300

    v 1.1.2

--- src/main/java/com/dilo/maven/quickstart/LoggingInDB.java
+++ src/main/java/com/dilo/maven/quickstart/LoggingInDB.java
@@ -10,61 +10,6 @@ public class LoggingInDB {
 	Connection connection = null;
 	Statement stmnt = null;
 	
-	/*if (isDbTableEmpty() == 0) {	
-	for (int i = 0; i < jsonStrs.size(); i++) {
-		String sql = "INSERT INTO json (data) VALUES ('" + jsonStrs.get(i) + "');";
-		stmnt.executeUpdate(sql);
-	}
-	return;
-} 
-
-String lasttsInDBsql = "select data -> 'timestamp' as lastTimestamp from json order by data-> 'timestamp' desc limit 1;";
-ResultSet rs3 = stmnt.executeQuery(lasttsInDBsql);
-while (rs3.next()) {
-	lasttsInDB = rs3.getString("lastTimestamp");
-}
-if (compareLognDB(logFile, lasttsInDB) != -1) {
-	//////
-}
-	private Integer isDbTableEmpty() throws SQLException {
-		int isNull = 0;
-		try {
-			rs = stmnt.executeQuery("select count (*) as total from json;");
-			while (rs.next()) {
-				isNull = rs.getInt("total");  
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return isNull;
-	}
-	private Integer compareLognDB(String logfile, String lasttsInDB)
-			throws FileNotFoundException {
-		String line = "";
-		int index = 0;
-
-		FileReader file = new FileReader(logfile);
-		try (BufferedReader br = new BufferedReader(file)) {
-			while (((line = br.readLine()) != null)) {
-
-				line = line.substring(line.indexOf("]") + 1);
-				String lasttsInLog = "\"" + line.substring(0, line.indexOf("[")) + "\"";
-				index++;
-				if (lasttsInDB.equalsIgnoreCase(lasttsInLog)) {
-					System.out.println("Logda:"+ lasttsInLog);
-					System.out.println("Dbde:"+lasttsInDB);
-					System.out.println("Logda "+ index + ".indexteyim!");
-					return index;
-				}
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return -1;
-	}
-*/
-
-	
 	public void createOp(LogAttributes la) throws SQLException {
 		
 		JsonHandler jh = new JsonHandler();
