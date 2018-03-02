600f83eadb7dee2427bbe0f7f3027e3612d64382
Dilşen Yıldar <dilsenyildar@gmail.com>
2018-03-02 23:44:29 +0300

    v 2.0.0

--- src/main/java/com/dilo/maven/quickstart/FileHandler.java
+++ src/main/java/com/dilo/maven/quickstart/FileHandler.java
@@ -6,14 +6,21 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashSet;
+import java.util.Iterator;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class FileHandler {
+	private Logger logger = LogManager.getLogger();
+	static HashSet<LogAttributes> hsAttr = new HashSet<LogAttributes>();
 	String logFile = "/home/dilo/eclipse-workspace/quickstart/logs/propertieslogs.log";
+
 	/**
 	 * @param hsAttr
 	 * @throws FileNotFoundException
 	 */
-	public void inputsFromLogFile(HashSet<LogAttributes> hsAttr) throws FileNotFoundException {
+	public void inputsFromLogFile() throws FileNotFoundException {
 		String line = "";
 		FileReader file = new FileReader(logFile);
 		try (BufferedReader br = new BufferedReader(file)) {
@@ -32,25 +39,42 @@ public class FileHandler {
 				hsAttr.add(la);
 			}
 		} catch (Exception e) {
+			logger.fatal(e);
 			e.printStackTrace();
 		}
 	}
-	
-	public void deleteLogFile() {
-		try{
-    		File file = new File(logFile);
-    		file.delete();
-    	}catch(Exception e){
-    		e.printStackTrace();
-    	}
+
+	public void addDB() {
+		LoggingInDB lidb = new LoggingInDB();
+		try {
+			Iterator<LogAttributes> hsAttrItr = hsAttr.iterator();
+			while (hsAttrItr.hasNext()) {
+				lidb.createOp(hsAttrItr.next());
+				hsAttrItr.remove();
+			}
+		} catch (Exception e) {
+			logger.error(e);
+			e.printStackTrace();
+		}
 	}
-	
+
+	public void deleteLogFile() {
+		try {
+			File file = new File(logFile);
+			file.delete();
+		} catch (Exception e) {
+			logger.fatal(e);
+			e.printStackTrace();
+		}
+	}
+
 	public void createLogFile() {
 		try {
-		      File file = new File(logFile);
-		      file.createNewFile();
-	    	} catch (IOException e) {
-		      e.printStackTrace();
+			File file = new File(logFile);
+			file.createNewFile();
+		} catch (IOException e) {
+			logger.fatal(e);
+			e.printStackTrace();
 		}
 	}
 }
