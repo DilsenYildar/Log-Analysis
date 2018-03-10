0e0bf3a173eac3a7dd5dc342f3fdd8aa6b926284
Dilşen Yıldar <dilsenyildar@gmail.com>
2018-03-10 13:33:58 +0300

    v 3.0.8

--- src/test/java/com/dilo/maven/quickstart/LoggingInDBTest.java
+++ src/test/java/com/dilo/maven/quickstart/LoggingInDBTest.java
@@ -71,7 +71,7 @@ public class LoggingInDBTest {
 			 * 2018-03-02 20:59:59.062 "}
 			 */
 			assertNotEquals(expected, result); // beklediğim format ile createOp fonk. çalıştıktan sonra dbye eklenen
-												// aynı değil...
+												// aynı değil..
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
