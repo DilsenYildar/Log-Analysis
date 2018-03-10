2aa4209197d2e0adca34edec40960cacbd9dc1e2
Dilşen Yıldar <dilsenyildar@gmail.com>
2018-03-10 12:32:08 +0300

    v 3.0.7

--- src/test/java/com/dilo/maven/quickstart/FileHandlerTest.java
+++ src/test/java/com/dilo/maven/quickstart/FileHandlerTest.java
@@ -13,20 +13,24 @@ import org.junit.Test;
 public class FileHandlerTest {
 	private FileHandler fh;
 	private File file;
+	private LogAttributes la;
+
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 		System.out.println("before FindHandlerClass");
 	}
-	
+
 	/**
 	 * Initializing Tests with @Before Methods
 	 *
 	 */
 	@Before
-	public void setUp(){
+	public void setUp() {
 		System.out.println("before");
 		fh = new FileHandler();
 		file = new File(fh.logFile);
+		la = new LogAttributes("[FATAL]", " 2018-03-02 20:59:59.062 ", "[main] CreateSampleLogFile ",
+				"This is a fatal message.");
 	}
 
 	/**
@@ -35,13 +39,27 @@ public class FileHandlerTest {
 	 */
 	@Test
 	public void CreateLogFile() {
-		
+
 		assertTrue(file.exists());
 		if (file.exists()) {
 			System.out.println("Propertieslogs.log mevcuttur.");
 		} else
 			fh.createLogFile();
 		System.out.println("Propertieslogs.log yaratıldı.");
+	}
+
+	/**
+	 * Test that the HashSet is null after the logs put into db, and test that the
+	 * hashset is not null after add a la object into hashset.
+	 */
+	@Test
+	public void inputsFromLogFile() {
+		assertTrue(fh.hsAttr.isEmpty()); // hashset is empty first.
+		System.out.println("Is the HashSet is empty? " + fh.hsAttr.isEmpty());
+
+		fh.hsAttr.add(la); // add to la object into HashSet
+		assertFalse(fh.hsAttr.isEmpty()); // hashset is not empty now.
+		System.out.println("Is the HashSet is empty? " + fh.hsAttr.isEmpty());
 	}
 
 	@After
