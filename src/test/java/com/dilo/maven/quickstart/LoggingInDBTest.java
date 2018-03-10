23c908809dca8c0218e702e1a74d437b97524964
Dilşen Yıldar <dilsenyildar@gmail.com>
2018-03-10 13:36:23 +0300

    v 3.0.8

--- src/test/java/com/dilo/maven/quickstart/LoggingInDBTest.java
+++ src/test/java/com/dilo/maven/quickstart/LoggingInDBTest.java
@@ -1,7 +1,6 @@
 package com.dilo.maven.quickstart;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.*;
 
 import java.io.IOException;
 import java.sql.Connection;
